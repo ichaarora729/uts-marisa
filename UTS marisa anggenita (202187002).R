c7fa7b4d627a027bbe9e84c10141d4134e15ccc5
ichaarora729 <149569665+ichaarora729@users.noreply.github.com>
2023-11-01 15:37:26 +0700

    Add files via upload

---
+++ UTS marisa anggenita (202187002).R
@@ -0,0 +1,5 @@
+data_set <-read.csv("C:/Users/USER/Documents/data/data_set.csv", sep = ";")
+data_set
+library(readxl)
+data_set <-read.csv("C:/Users/USER/Documents/data/data_set.csv", sep = ";")
+View(data_set)
